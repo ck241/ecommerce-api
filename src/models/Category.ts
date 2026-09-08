72ca3f986f5ff925ef22b3b43e780ddd0aa55e8a
ck241 <295834703+ck241@users.noreply.github.com>
2026-09-08 16:16:24 +0200

    feat: add category model and validation schemas

---
+++ src/models/Category.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Category = model("Category", categorySchema);
