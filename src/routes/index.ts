249bf988f8abfd379949b9ca68e9ff312efe1c4c
ck241 <295834703+ck241@users.noreply.github.com>
2026-09-09 13:41:56 +0200

    add orders CRUD endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { categoryRouter } from "./categoryRoutes.ts";
+export { orderRouter } from "./orderRoutes.ts";
 export { productRouter } from "./productRoutes.ts";
 export { userRouter } from "./userRoutes.ts";
